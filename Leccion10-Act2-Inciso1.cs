760e647d1e4a8fd61fd989f410dfba1b9c87f6ec
23CCL01-ChristianCarrillo <ccarrillodb23@gmail.com>
2026-05-05 17:48:27 -0600

    Add files via upload

---
+++ Leccion10-Act2-Inciso1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i, result;
+
+        for (i = 1; i <= 20; i++)
+        {
+            result = 4 * i;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Leccion10-Act2-Inciso1.psc
@@ -0,0 +1,8 @@
+Algoritmo Multiplos
+    Definir i, result Como Entero
+    
+    Para i <- 1 Hasta 20 Hacer
+        result <- 4 * i
+        Escribir result
+    FinPara
+FinAlgoritmo
